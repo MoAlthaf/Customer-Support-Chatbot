8eb19bff970a065f07cab4c80df7556cbd23364a
MoAlthaf <60300344@udst.edu.qa>
2026-02-17 23:22:33 +0300

    refactor: Add deployment support

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from chatbot import get_response
+import os
 
 app = Flask(__name__)
 
@@ -46,4 +47,5 @@ def reset():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
